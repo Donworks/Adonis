1f4a46ccb8a435fd58be43ec08becabd77de1f5c
988990 <S7.3redseatsamg777@gmail.com>
2023-09-06 21:52:11 -0400

    addch2 exercises

--- Exercises/ch02/example1.cpp
+++ Exercises/ch02/example1.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+using namespace std;
+
+int main()
+{
+    int hour, minute;
+    hour = 11;
+    minute = 59;
+    cout << " Number of minutes since midnight: ";
+    cout << hour * 60 + minute << endl;
+    cout << " Fraction of the hour that has passed: ";
+    cout << minute / 60 << endl;
+    return 0;
+}
---
+++ Exercises/ch02/example2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i1, i2, i3;
+    i1 = 5;
+    i2 = 2;
+    i3 = -5;
+    cout << "with integer division 5 divided by 2 is " << i1 / i2 << ", ";
+    cout << "and -5 divided by 2 is" << i3 / i2 << '.' << endl;
+    return 0;
+}
