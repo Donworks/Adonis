666959e1f17fc835f80d47b01ed26d8e97f8ec67
988990 <S7.3redseatsamg777@gmail.com>
2023-09-21 00:32:18 -0400

    add ive been tweaking some of the code

---
+++ Exercises/ch07/Extrac_Char_from_aString.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string fruit = " banana ";
+    char letter = fruit[1];
+    cout << letter << endl;
+    return 0;
+}
---
+++ Exercises/ch07/S70.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    string str1;
+    str1 = "S70 AMG///, ";
+    string str2 = " Is one of the fastest cars in the world so much to the point they put the legenday m104 aka engine in a pagani zonda!";
+    cout << str1 << str2 << endl;
+    return 0;
+}
